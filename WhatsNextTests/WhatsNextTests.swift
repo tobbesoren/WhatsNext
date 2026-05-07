901bcb616770f974ef317ed7a5a1b4080e4c19ed
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2026-05-07 16:04:11 +0200

    First commit

---
+++ WhatsNextTests/WhatsNextTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNextTests.swift
+//  WhatsNextTests
+//
+//  Created by Tobias Sörensson on 2026-05-07.
+//
+
+import Testing
+@testable import WhatsNext
+
+struct WhatsNextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
